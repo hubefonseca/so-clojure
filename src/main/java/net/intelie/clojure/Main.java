900f2526b452df3d4b0bce86c84c74c27d35fe48
Hubert Fonseca <hubefonseca@gmail.com>
2010-02-10 17:40:59 -0200

    test calling clojure from java

--- src/main/java/net/intelie/clojure/Main.java
+++ src/main/java/net/intelie/clojure/Main.java
@@ -12,14 +12,21 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         // Load the Clojure script -- as a side effect this initializes the runtime.
-        RT.loadResourceScript("net/intelie/clojure/foo.clj");
+//        RT.loadResourceScript("net/intelie/clojure/foo.clj");
+//
+//        // Get a reference to the foo function.
+//        Var foo = RT.var("net.intelie.clojure.foo", "foo");
+//
+//        // Call it!
+//        Object result = foo.invoke("Hi", "there");
+//        System.out.println(result);
 
-        // Get a reference to the foo function.
-        Var foo = RT.var("net.intelie.clojure.foo", "foo");
 
-        // Call it!
-        Object result = foo.invoke("Hi", "there");
-        System.out.println(result);
+        net.intelie.clojure.random g = new net.intelie.clojure.random();
+
+        System.out.println(g.rdm());
+        System.out.println(g.rdm());
+        System.out.println(g.rdm());
     }
     
 }
