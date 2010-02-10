0f83f25c825222f3ee8b0d17bc8c8153820e5ad0
Hubert Fonseca <hubefonseca@gmail.com>
2010-02-10 16:56:50 -0200

    first commit

---
+++ src/main/clojure/net/intelie/clojure/foo.clj
@@ -0,0 +1,5 @@
+; foo.clj
+(ns net.intelie.clojure.foo)
+
+(defn foo [a b]
+  (str a " " b))
---
+++ src/main/java/net/intelie/clojure/Main.java
@@ -0,0 +1,25 @@
+package net.intelie.clojure;
+
+import clojure.lang.RT;
+import clojure.lang.Var;
+
+
+/**
+ * @author hubertfonseca
+ */
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        // Load the Clojure script -- as a side effect this initializes the runtime.
+        RT.loadResourceScript("net/intelie/clojure/foo.clj");
+
+        // Get a reference to the foo function.
+        Var foo = RT.var("net.intelie.clojure.foo", "foo");
+
+        // Call it!
+        Object result = foo.invoke("Hi", "there");
+        System.out.println(result);
+    }
+    
+}
